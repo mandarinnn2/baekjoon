176db447ef974f05016ee1881107bfff6a3a0c9e
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2024-12-30 17:06:44 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1108 ms, Memory: 60960 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,37 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 예제의 첫번째 수를 받아온다
 
-        for(int i = 0; i < N; i++){
-            arr[i] = sc.nextInt();
+        int suNo = Integer.parseInt(st.nextToken());
+        int quizNo = Integer.parseInt(st.nextToken());
+
+        long[] S = new long[suNo + 1];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= suNo; i++){
+            S[i] = S[i-1]+ Integer.parseInt(st2.nextToken());
         }
 
-        int[] ans = new int[N];
-        
-        for(int i = 0; i < N; i++){
-            if(i == 0){
-                ans[i] = arr[i];
-            }else{
-                ans[i] = arr[i] + ans[i-1];
-            }
-        }
-
-        for(int i = 0; i < M; i++){
-            int A = sc.nextInt() - 1;
-            int B = sc.nextInt() - 1;
-            int result = 0;
-            if(A==0){
-                result = ans[B];
-            }else{
-                result = ans[B] - ans[A-1];
-            }
-            System.out.println(result);
+        for(int i = 1; i <= quizNo; i++){
+            StringTokenizer st3 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st3.nextToken());
+            int b = Integer.parseInt(st3.nextToken());
+            System.out.println(S[b] - S[a-1]);
         }
     }
 }
