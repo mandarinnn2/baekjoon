1ad93b9161da90e0e37e9b371b8d041ffb880506
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2025-01-09 20:02:07 +0900

    [Silver I] Title: 절댓값 힙, Time: 432 ms, Memory: 26040 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,36 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> MyQueue = new PriorityQueue<>((o1, o2) ->{
-            // 절댓값 작은 데이터 우선
-            // 절댓값이 같은 경우 음수 우선
+        
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1, o2) -> {
             int first_abs = Math.abs(o1);
             int second_abs = Math.abs(o2);
-            if(first_abs == second_abs)
+            if(first_abs == second_abs){
                 return o1 > o2 ? 1 : -1;
-            
+            }
             return first_abs - second_abs;
-            // 음수 or 양수로 비교 정렬 기준 세우기
         });
 
         for(int i = 0; i < N; i++){
-            int request = Integer.parseInt(br.readLine());
-            if(request == 0){
-                if(MyQueue.isEmpty())
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0){
+                if(myQueue.isEmpty()){
                     System.out.println("0");
-                else
-                    System.out.println(MyQueue.poll());
+                }else{
+                    System.out.println(myQueue.poll());
+                }
             }else{
-                MyQueue.add(request);
+                myQueue.add(x);
             }
         }
-
     }
 }
