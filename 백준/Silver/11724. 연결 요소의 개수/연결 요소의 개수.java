bdfe783d81c586b4b7722a7b17a663fc13dc1dd1
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2025-01-16 16:19:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 142128 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -4,27 +4,29 @@ import java.util.*;
 public class Main {
     static boolean visited[];
     static ArrayList<Integer> A[];
-    public static void main(String[] args) throws IOException{
+   public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); 
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        A = new ArrayList[N + 1];
-
         visited = new boolean[N + 1];
+
+        A = new ArrayList[N + 1];
         for(int i = 1; i < N + 1; i++){
             A[i] = new ArrayList<Integer>();
         }
 
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            A[s].add(e);
-            A[e].add(s);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            A[x].add(y);
+            A[y].add(x);
         }
+
         int count = 0;
         for(int i = 1; i < N + 1; i++){
             if(!visited[i]){
@@ -32,9 +34,11 @@ public class Main {
                 DFS(i);
             }
         }
+
         System.out.println(count);
-    }
-    public static void DFS(int v){
+
+   } 
+   public static void DFS(int v){
         if(visited[v]){
             return;
         }
@@ -44,5 +48,5 @@ public class Main {
                 DFS(i);
             }
         }
-    }
+   }
 }
