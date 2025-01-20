248038dde9718dec16bfaf658d1b950a9ccf8dd7
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2025-01-20 11:17:03 +0900

    [Silver IV] Title: 수 찾기, Time: 1040 ms, Memory: 50864 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,54 +1,57 @@
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
+
+    static int N;
+    static int M;
+    static int A[];
+    static int B[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String x = br.readLine();
-        int N = Integer.parseInt(x);
 
-        String y = br.readLine();
-        StringTokenizer st = new StringTokenizer(y);
-
-        int[] A = new int[N];
-        int i = 0;
-        while(st.hasMoreTokens()){
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
-            i++;
         }
 
-        String a = br.readLine();
-        int M = Integer.parseInt(a);
-
-        String q = br.readLine();
-        StringTokenizer st1 = new StringTokenizer(q);
-
-        int[] B = new int[M];
-        i = 0;
-        while(st1.hasMoreTokens()){
-            B[i] = Integer.parseInt(st1.nextToken());
-            i++;
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        B = new int[M];
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(A);
 
-        for(i = 0; i < M; i++){
-            int start = 0;
-            int end = A.length - 1;
-            int idx = 0;
-            while(start <= end){
-                int mid = (start + end) / 2;
-                if(A[mid] == B[i]){
-                    idx = 1;
-                    break;
-                }else if(B[i] < A[mid]){
-                    end = mid - 1;
-                }else{
-                    start = mid + 1;
-                }
+        for (int i = 0; i < M; i++) {
+            System.out.println(comp(B[i]));
+        }
+    }
+
+    public static int comp(int a){
+        int start = 0;
+        int end = A.length - 1;
+        boolean x = false;
+        while(start <= end){
+            int mid = (end + start) / 2;
+
+            if(A[mid] > a){
+                end = mid - 1;
+            }else if(A[mid] < a){
+                start = mid + 1;
+            }else{
+                x = true;
+                break;
             }
-            System.out.println(idx);
+        }
+        
+        if(x){
+            return 1;
+        }else{
+            return 0;
         }
     }
 }
