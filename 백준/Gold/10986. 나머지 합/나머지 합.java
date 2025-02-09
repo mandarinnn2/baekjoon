443ae728ef941123fda4b32e66b8c97ffdb7bec0
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2025-02-09 12:47:09 +0900

    [Gold III] Title: 나머지 합, Time: 452 ms, Memory: 120656 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -5,35 +5,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        long A[] = new long[N];
-        long X[] = new long[M];
         st = new StringTokenizer(br.readLine());
-        A[0] = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < N; i++) {
-            A[i] = A[i - 1] + Integer.parseInt(st.nextToken());
-        }
-
+        long A[] = new long[N];
+        long B[] = new long[M];
+        long x = 0;
         long answer = 0;
         for (int i = 0; i < N; i++) {
-            A[i] %= M;
-            ++X[(int)A[i]];
-            if (A[i] == 0) {
-                ++answer;
-            }
+            x += Integer.parseInt(st.nextToken());
+            A[i] = (int)(x % M);
+            if(A[i] == 0) ++answer;
         }
 
-        for (int i = 0; i < M; i++) {
-            if (X[i] == 2) {
-                ++answer;
-            } else if (X[i] > 2) {
-                answer = answer + (X[i] * (X[i] - 1)) / 2;
-            }
+        for(int i = 0; i < N; i++){
+            ++B[(int)(A[i])];
         }
 
+        for(int i = 0; i < M; i++){
+            if(B[i] >= 2){
+                answer += B[i] * (B[i] - 1) / 2;
+            }
+        }
+        
         System.out.println(answer);
+
     }
 }
