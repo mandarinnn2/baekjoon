30516e134b31d009ae8ee3e15c087005709908f2
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2025-02-13 15:30:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 596 ms, Memory: 141876 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,31 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean visited[];
-    static ArrayList<Integer> A[];
-   public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // 방문 여부 확인
+    public static boolean[] visited;
 
+    // 각 노드별 연결 노드 확인
+    public static ArrayList<Integer>[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 노드와 간선 수 입력받기
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        A = new ArrayList[N + 1];
         visited = new boolean[N + 1];
 
-        A = new ArrayList[N + 1];
-        for(int i = 1; i < N + 1; i++){
+        // 각 노드와 연결된 노드 작성을 위해 각 ArrayList를 또 하나의 ArrayList로 초기화하기
+        for (int i = 1; i < N + 1; i++) {
             A[i] = new ArrayList<Integer>();
         }
 
+        // 값을 입력받아서 각 노드와 연결되어 있는 ArrayList에 저장하기
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            A[x].add(y);
-            A[y].add(x);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            A[u].add(v);
+            A[v].add(u);
         }
 
+        // DFS 메소드를 재귀형식으로 돌며 방문하지 않은 노드를 확인하여 연결 요소의 수를 구한다.
         int count = 0;
         for(int i = 1; i < N + 1; i++){
             if(!visited[i]){
@@ -36,17 +43,19 @@ public class Main {
         }
 
         System.out.println(count);
+    }
 
-   } 
-   public static void DFS(int v){
-        if(visited[v]){
+    public static void DFS(int a){
+        // 이미 방문한 경우 return
+        if(visited[a])
             return;
-        }
-        visited[v] = true;
-        for(int i: A[v]){
+
+        // 방문 여부를 true로 변경하고 해당 노드와 연결된 모든 노드를 확인한다.
+        visited[a] = true;
+        for(int i: A[a]){
             if(!visited[i]){
                 DFS(i);
             }
         }
-   }
+    }
 }
